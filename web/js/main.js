88bd8620687c65706843b18e3dd4ca822caa0c36
Sreejith Unnikrishnan <'sreejithu393@gmail.com'>
2015-03-23 15:12:51 -0400

    added list from saved searches

--- web/js/main.js
+++ web/js/main.js
@@ -5,48 +5,91 @@
  */
 
 $(document).ready(function () {
-$('#goSearch').click(function () {
-if ($('#search').val() === ''){
-    alert("please enter search string");
-    console.log("please enter search string");
-}
-else{
-    var type = $('#type').val();
-        if (type === 'all') {
-            var url = './webresources/search/' + $('#search').val();
-            $.getJSON(url, function (data) {
-            if (data && data !== null) {
-            var outHtml = "";
-                    for (var i = 0; i < data.length; i++)
-                    outHtml += "<p>" + data[i].name + "&nbsp<button id=\"save\" onclick=\"save(" + data[i].id + ")\">Save</button></p>"
-                    + "<input type=\"hidden\" id=\"hid\" value=\"" + data[i].id + "\"/>"
-                    + "<p>" + data[i].address + "</p>"
-                    + "<p>" + data[i].phone + "</p>";
+    $('#goSearch').click(function () {
+        if ($('#search').val() === '') {
+            alert("please enter search string");
+            console.log("please enter search string");
+        }
+        else {
+            var type = $('#type').val();
+            if (type === 'all') {
+                var url = './webresources/search/' + $('#search').val();
+                $.getJSON(url, function (data) {
+                    if (data && data !== null) {
+                        var outHtml = "";
+                        for (var i = 0; i < data.length; i++)
+                            outHtml += "<p>" + data[i].name + "&nbsp<button id=\"save\" onclick=\"save(" + data[i].id + ")\">Save</button></p>"
+                                    + "<input type=\"hidden\" id=\"hid\" value=\"" + data[i].id + "\"/>"
+                                    + "<p>" + data[i].address + "</p>"
+                                    + "<p>" + data[i].phone + "</p>";
+                    }
+                    else {
+                        var outHtml = "<p>No Results found</p>";
+                    }
+                    $('#output').html(outHtml);
+                });
             }
             else {
-                var outHtml = "<p>No Results found</p>";
-            }
-                $('#output').html(outHtml);
-            });
-            }
-        else {
-            var url = './webresources/search/' + $('#search').val() + '/' + type;
-                    $.getJSON(url, function (data) {
+                var url = './webresources/search/' + $('#search').val() + '/' + type;
+                $.getJSON(url, function (data) {
                     var outHtml = "";
-                            for (var i = 0; i < data.length; i++)
-                            //id = data[i].id;
-                            outHtml += "<p>" + data[i].name + "<button id=\"details\">Details</button></p>"
-                            + "<input type=\"hidden\" id=\"hid\" value=\"" + data[i].id + "\"/>";
-                            $('#output').html(outHtml);
-                    });
+                    for (var i = 0; i < data.length; i++)
+                        //id = data[i].id;
+                         outHtml += "<p>" + data[i].name + "&nbsp<button id=\"save\" onclick=\"save(" + data[i].id + ")\">Save</button></p>"
+                                    + "<input type=\"hidden\" id=\"hid\" value=\"" + data[i].id + "\"/>"
+                                    + "<p>" + data[i].address + "</p>"
+                                    + "<p>" + data[i].phone + "</p>";
+                    $('#output').html(outHtml);
+                });
             }
+        }
+    });
+
+
+    $('#goSaved').click(function () {
+        if ($('#savedSearch').val() === '') {
+            alert("please enter search string");
+            console.log("please enter search string");
+        }
+        else {
+            var type = $('#typeSaved').val();
+            if (type === 'all') {
+                var url = './webresources/saved/' + $('#savedSearch').val();
+                $.getJSON(url, function (data) {
+                    if (data && data !== null) {
+                        var outHtml = "";
+                        for (var i = 0; i < data.length; i++)
+                            outHtml += "<p>" + data[i].name + "</p>"
+                                    + "<input type=\"hidden\" id=\"hid\" value=\"" + data[i].id + "\"/>"
+                                    + "<p>" + data[i].address + "</p>"
+                                    + "<p>" + data[i].phone + "</p>";
+                    }
+                    else {
+                        var outHtml = "<p>No Results found</p>";
+                    }
+                    $('#output').html(outHtml);
+                });
             }
+            else {
+                var url = './webresources/saved/' + $('#savedSearch').val() + '/' + type;
+                $.getJSON(url, function (data) {
+                    var outHtml = "";
+                    for (var i = 0; i < data.length; i++)
+                        //id = data[i].id;
+                        outHtml += "<p>" + data[i].name + "</p>"
+                                    + "<input type=\"hidden\" id=\"hid\" value=\"" + data[i].id + "\"/>"
+                                    + "<p>" + data[i].address + "</p>"
+                                    + "<p>" + data[i].phone + "</p>";
+                    $('#output').html(outHtml);
+                });
+            }
+        }
     });
 });
 
 
 function save(id) {
-    console.log("id:" +id);
+    console.log("id:" + id);
 //    var url = './webresources/saved/';
 //                    $.post(url, id, function (data) {
 //                    var outHtml = "";
@@ -55,12 +98,12 @@ function save(id) {
 //                            $('#output').html(outHtml);
 //                    });
     $.ajax({
-       url: 'webresources/saved',
-       method: 'POST',
-       data: { 'id' : id },
-       success: function(data) {
-           $('#output').text(data);
-       }
+        url: 'webresources/saved',
+        method: 'POST',
+        data: {'id': id},
+        success: function (data) {
+            $('#output').text(data);
+        }
     });
 }
 
